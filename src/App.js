c6a1a467d09b82a3e7fb82ef6c3022518300751c
L Phansalkar <lphansalkar@Ls-MacBook-Air.local>
2024-08-16 22:00:21 -0700

    links are nice and formatted now and link to current available sites

--- src/App.js
+++ src/App.js
@@ -5,16 +5,42 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="text">
-        <h1>
-          L Phansalkar (they/them)
-        </h1>
+        <div className="topstuff">
         <h2>
-          Technical Support Engineer and Professional Speaker
+          L Phansalkar (they/them)
         </h2>
-      <h3>!**website coming soon**!</h3>
+        <h3>
+          Technical Support Engineer and Professional Speaker
+        </h3>
         </div>
       </header>
+<div className="content">
+  <h2 className="link">Links</h2>
+  <p></p>
+  <a href="https://www.linkedin.com/events/outintechpride-alltogethernow7202049812949549056/theater/" target="_blank">download my <b>resume</b></a>
+  <p></p>
+  
+  <p></p>
+  <a href= "https://www.linkedin.com/events/outintechpride-alltogethernow7202049812949549056/theater/" target="_blank">watch my<i> Out In Tech</i> <b> Pride Keynote Speech </b> (June 2024)</a>
+  <p></p>
+  
+  <p></p>
+  <a href="https://web.archive.org/web/20150630183736/http://www.outerplaces.com/tag?tag=Technology" target="_blank"> read my <i>Outer Places</i> <b> Online Science & Technology News Publications </b>(2015)</a>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <a href=  "http://apadivision16.org/wp-content/uploads/2021/03/TSP-Spring-2021.pdf#page=21" target="_blank"> read my <i>The School Psychologist</i> <b> Journal Publication </b> (Spring 2021)</a>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  lorem ipsuem
+  <p></p>
+
+</div>
       </div>
   );
 }
