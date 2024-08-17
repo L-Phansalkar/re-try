50c1cf3182a8ec1fc29470b4a5f7a02e5df7c1c6
L Phansalkar <lphansalkar@Ls-MacBook-Air.local>
2024-08-16 22:45:04 -0700

    no referrer

--- src/App.js
+++ src/App.js
@@ -21,16 +21,16 @@ function App() {
   <p></p>
   
   <p></p>
-  <a href= "https://www.linkedin.com/events/outintechpride-alltogethernow7202049812949549056/theater/" target="_blank" rel="noreferrer">watch my<i> Out In Tech</i> <b> Pride Keynote Speech </b> (June 2024)</a>
+  <a href= "https://www.linkedin.com/events/outintechpride-alltogethernow7202049812949549056/theater/" >watch my<i> Out In Tech</i> <b> Pride Keynote Speech </b> (June 2024)</a>
   <p></p>
   
   <p></p>
-  <a href=  "http://apadivision16.org/wp-content/uploads/2021/03/TSP-Spring-2021.pdf#page=21" target="_blank" rel="noreferrer"> read my <i>The School Psychologist</i> <b> Journal Publication </b> (Spring 2021)</a>
+  <a href=  "http://apadivision16.org/wp-content/uploads/2021/03/TSP-Spring-2021.pdf#page=21" > read my <i>The School Psychologist</i> <b> Journal Publication </b> (Spring 2021)</a>
   <p></p>
   <p></p>
   <p></p>
   <p></p>
-  <a href="https://web.archive.org/web/20150630183736/http://www.outerplaces.com/tag?tag=Technology" target="_blank" rel="noreferrer"> read my <i>Outer Places</i> <b> Online Science & Technology News Publications </b>(2015)</a>
+  <a href="https://web.archive.org/web/20150630183736/http://www.outerplaces.com/tag?tag=Technology" > read my <i>Outer Places</i> <b> Online Science & Technology News Publications </b>(2015)</a>
   <p></p>
   <p></p>
   <p></p>
