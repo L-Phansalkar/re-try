b44e95a065a0e6bfbc7d5a26384fb40eb83237df
L Phansalkar <lphansalkar@Ls-MacBook-Air.local>
2024-08-16 18:34:58 -0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+//import headshot from './headshot.jpeg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="text">
+        <h1>
+          L Phansalkar (they/them)
+        </h1>
+        <h2>
+          Technical Support Engineer and Professional Speaker
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to some coding project
         </a>
+        </div>
       </header>
-    </div>
+      </div>
   );
 }
 
