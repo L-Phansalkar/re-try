102d873d13a8930a6ca83d6aba4826266469aaa5
L Phansalkar <lphansalkar@Ls-MacBook-Air.local>
2024-08-16 22:43:59 -0700

    back to normal

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <h2>
           L Phansalkar (they/them)
         </h2>
+        <h3>
+          Technical Support Engineer and Professional Speaker
+        </h3>
         </div>
       </header>
 <div className="content">
@@ -22,12 +25,12 @@ function App() {
   <p></p>
   
   <p></p>
-  <a href="https://web.archive.org/web/20150630183736/http://www.outerplaces.com/tag?tag=Technology" target="_blank" rel="noreferrer"> read my <i>Outer Places</i> <b> Online Science & Technology News Publications </b>(2015)</a>
-  <p></p>
-  <p></p>
-  <p></p>
-  <p></p>
   <a href=  "http://apadivision16.org/wp-content/uploads/2021/03/TSP-Spring-2021.pdf#page=21" target="_blank" rel="noreferrer"> read my <i>The School Psychologist</i> <b> Journal Publication </b> (Spring 2021)</a>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p></p>
+  <a href="https://web.archive.org/web/20150630183736/http://www.outerplaces.com/tag?tag=Technology" target="_blank" rel="noreferrer"> read my <i>Outer Places</i> <b> Online Science & Technology News Publications </b>(2015)</a>
   <p></p>
   <p></p>
   <p></p>
